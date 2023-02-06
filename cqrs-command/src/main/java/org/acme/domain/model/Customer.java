00b7f01dc31d2f400c51f45a5e4306d71884613b
ffauvelet <fabien.fauvelet@gmail.com>
2023-02-06 11:33:33 +0100

    Modified domains object's "equals" function behavior to compare only UUIDs

--- cqrs-command/src/main/java/org/acme/domain/model/Customer.java
+++ cqrs-command/src/main/java/org/acme/domain/model/Customer.java
@@ -4,9 +4,12 @@ import java.time.LocalDate;
 import java.util.UUID;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Customer {
+    @EqualsAndHashCode.Include
     private UUID id;
     private String firstName;
     private String lastName;
--- cqrs-command/src/main/java/org/acme/domain/model/Resource.java
+++ cqrs-command/src/main/java/org/acme/domain/model/Resource.java
@@ -1,11 +1,14 @@
 package org.acme.domain.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.UUID;
 
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Resource {
+    @EqualsAndHashCode.Include
     private UUID id;
     private String name;
     private String type;
